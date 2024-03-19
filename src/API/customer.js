1376b0f922869b7fc93fcf9edbcf9cc453755675
dozdemir <ozdemirdilan@hotmail.com>
2024-03-19 23:18:54 +0300

    .

--- src/API/customer.js
+++ src/API/customer.js
@@ -1,32 +1,38 @@
 import axios from "axios";
 
 export const getCustomers = async () => {
-    const { data } = await axios.get("http://localhost:8080/customer/find-all");
-    return data;
-}
+  const { data } = await axios.get(
+    import.meta.env.VITE_APP_BASE_URL + "/customer/find-all"
+  );
+  return data;
+};
 
 export const deleteCustomer = async (id) => {
-    const {data} = await axios.delete(
-        `http://localhost:8080/customer/delete/${id}`);
-    return data;
-}
+  const { data } = await axios.delete(
+    `${import.meta.env.VITE_APP_BASE_URL}/customer/delete/${id}`
+  );
+  return data;
+};
 
 export const createCustomer = async (customer) => {
-    const {data} = await axios.post(
-        `http://localhost:8080/customer/save`, customer
-    );
-    return data;
-}
+  const { data } = await axios.post(
+    `${import.meta.env.VITE_APP_BASE_URL}/customer/save`,
+    customer
+  );
+  return data;
+};
+
+export const updateCustomerFunction = async (customer) => {
+  const { data } = await axios.put(
+    `${import.meta.env.VITE_APP_BASE_URL}customer/update/${customer.id}`,
+    customer
+  );
+  return data;
+};
 
 export const getCustomerById = async (id) => {
-    const { data } = await axios.get(`http://localhost:8080/customer/${id}`);
-    return data;
-  };
-
-  export const updateCustomerFunction = async (customer) => {
-  const { data } = await axios.put(
-    `http://localhost:8080/customer/update/${customer.customerId}`,
-    customer
+  const { data } = await axios.get(
+    `${import.meta.env.VITE_APP_BASE_URL}/customer/${id}`
   );
   return data;
 };
